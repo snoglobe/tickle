ddc5f8b6d82239a717450d59f8d914a3e56bc1d2
Mason Meirs <maseymoose@gmail.com>
2022-01-04 19:29:35 -0500

    pallete and tixel

---
+++ palette.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define BLACK   0x222533ff
+#define DGREY   0x6275baff
+#define LGREY   0xa3c0e6ff
+#define WHITE   0xfafffcff
+#define BEIGE   0xffab7bff
+#define PINK    0xff6c7aff
+#define RED     0xdc435bff
+#define BLUE    0x3f48c2ff
+#define LBLUE   0x448de7ff
+#define GREEN   0x2bdb72ff
+#define LGREEN  0xa7f547ff
+#define YELLOW  0xffeb33ff
+#define GOLD    0xf58931ff
+#define ORANGE  0xdb4b3dff
+#define MAROON  0xa63d57ff
+#define DPURPLE 0x36354dff
