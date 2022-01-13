08f3f502a6c6053761c6ee2ece1beee2b80a8d41
Shane Paton <45543346+shanepaton@users.noreply.github.com>
2022-01-13 00:04:31 -0600

    Some house keeping and publicized my Windows build script
    Changes
        ~ build -> linux-build
        - editor/sprite/Filename
        - main2.c

---
+++ editor/sound/windows-build.bat
@@ -0,0 +1,2 @@
+C:\raylib\w64devkit\bin\gcc main.c -o tixfx.exe -Wno-missing-braces -s -O1 -I. -Wl,--subsystem,windows -lraylib -lopengl32 -lgdi32 -lwinmm -static -lpthread -DPLATFORM_DESKTOP
+tixfx.exe
---
+++ windows-build.bat
@@ -0,0 +1,2 @@
+C:\raylib\w64devkit\bin\gcc main.c -Wall -Wno-missing-braces -s -ljim -lraylib -lopengl32 -lgdi32 -lwinmm -lpthread -static -o tickle.exe
+tickle.exe
